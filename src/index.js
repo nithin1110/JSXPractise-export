710851e54e591760f4552ffb49dbf25f96686c06
Nithin <106112831+nithin1110@users.noreply.github.com>
2023-08-17 18:38:00 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>My Friends</h1>,
+    <ol>
+      <li>Nikki</li>
+      <li>Nithin</li>
+      <li>Daddy</li>
+    </ol>
+    <ul>
+      <li>Navya</li>
+      <li>Pranavi</li>
+      <li>Nandy</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
